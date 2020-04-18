53aefe085b4af86281de83ad1e44cb5bec89a587
asseysanzen <asseysanzen@gmail.com>
2020-04-18 17:27:30 +0900

    commit 4/18

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,14 @@
+class BooksController < ApplicationController
+	def home
+	end
+	def about
+	end
+	def new
+	end
+	def create
+	end
+	def index
+	end
+	def show
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :books, only: [:new, :create, :index, :show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'books#index'
+  get 'home' => 'books#home'
+  get 'home/about' => 'books#about'
 end
