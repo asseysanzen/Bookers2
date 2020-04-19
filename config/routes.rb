9de8550f68daea929bf2fad6dce06a3f625b0360
asseysanzen <asseysanzen@gmail.com>
2020-04-19 22:09:51 +0900

    commit 4/19(3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :books
-  devise_for :users
+  devise_for :users, controllers: {   registrations: 'users/registrations', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'books#index'
-  get 'home' => 'books#home'
+  root 'books#home'
   get 'home/about' => 'books#about'
   resources :users, only: [:show, :index, :edit, :update, :create]
 end
