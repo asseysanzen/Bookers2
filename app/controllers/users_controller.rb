a92f558b7185c7d6fa1de510d60d2f93440a6f31
asseysanzen <asseysanzen@gmail.com>
2020-04-20 16:58:25 +0900

    commit 4/20(2)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,11 @@ class UsersController < ApplicationController
   end
   def update
   	@user = User.find(params[:id])
-  	@user.update(user_params)
-  	redirect_to user_path(@user.id), flash:{update: "You have updated book successfully!"}
+  	if @user.update(user_params)
+  	 redirect_to user_path(@user.id), flash:{update: "You have updated book successfully!"}
+    else
+      render :edit
+    end
   end
   private
   def user_params
